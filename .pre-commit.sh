ef5768ed9af188e99bcf1943880a4dc5c0f2a91d
Philippe Prados <github@prados.fr>
2024-12-13 15:58:01 +0100

    Update document_loader_pdf.ipynb

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ $(git rev-parse --abbrev-ref HEAD) == "master" ]; then
     # make validate
-    echo "Don't forget to call `make validate`"
+    echo "Don't forget to call 'make validate'"
 fi
